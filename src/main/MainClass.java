8e55a19dde14ea8e82c668b703c144d77ee5ad45
dbswjd07 <10823syn@naver.com>
2023-06-02 16:44:58 +0900

    Main 수정본

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,5 +1,55 @@
 package main;
 
-public class MainClass {
+import java.util.HashMap;
+import java.util.Scanner;
 
+import member.MemberDTO;
+import member.MemberService;
+
+public class MainClass {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	MemberService m = new MemberService();
+	HashMap<String, String> map = new HashMap<>();
+	
+	String name;
+	int age,num;
+	
+	while(true) {
+			System.out.println("====================");
+			System.out.println("1.회원가입");
+			System.out.println("2.회원보기");
+			System.out.println("3.회원삭제");
+			System.out.println("4.시스템종료");
+			System.out.println("====================");
+			System.out.print(">>> ");
+			age = input.nextInt();
+		
+		switch(age) {
+			case 1: 
+				MemberDTO dto = new MemberDTO();
+				System.out.print("ID 입력 : ");
+				String ID = input.next();
+				System.out.print("PWD 입력 : ");
+				String PWD = input.next();
+				
+				dto.setUserId(ID);
+				dto.setUserId(PWD);
+				map = m.register();	
+				
+				break;
+			case 2: 
+				System.out.println("ID : ");
+				System.out.println("PWD : ");
+				break;
+			case 3: 
+				
+				break;
+			case 4: 
+				System.out.println("시스템을 종료합니다");			
+				return;
+		}
+	}
+	
+	}
 }

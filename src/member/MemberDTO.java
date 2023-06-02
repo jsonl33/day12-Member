e2ee99219d6b6fa3fb5047181851ecbeb9617eb1
testuser <testuser@test.com>
2023-06-02 16:27:57 +0900

    member dto, service

--- src/member/MemberDTO.java
+++ src/member/MemberDTO.java
@@ -1,5 +1,22 @@
 package member;
 
 public class MemberDTO {
-
+	String userId;
+	String userPwd;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	
+	
+	
 }
--- src/member/MemberService.java
+++ src/member/MemberService.java
@@ -1,5 +1,16 @@
 package member;
 
-public class MemberService {
+import java.util.HashMap;
 
+public class MemberService {
+	MemberDTO dto = new MemberDTO();
+	
+	public HashMap<String, String> register(){
+		HashMap<String, String> member = new HashMap<>();
+		String userId = dto.getUserId();
+		String userPwd = dto.getUserPwd();
+		
+		member.put(userId, userPwd);
+		return member;
+	}
 }
